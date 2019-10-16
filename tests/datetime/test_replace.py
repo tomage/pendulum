43954db528ba768f1fda0f08e5895bd35e28d2e7
Tómas Árni Jónasson <mail@tomas.arni.jonasson.name>
2019-10-16 10:16:53 +0000

    fix: fmt (black)

--- tests/datetime/test_replace.py
+++ tests/datetime/test_replace.py
@@ -5,7 +5,7 @@ from ..conftest import assert_datetime
 
 def test_replace_tzinfo_dls_off():
     utc = pendulum.datetime(2016, 3, 27, 0, 30)  # 30 min before DLS turning on
-    in_paris = utc.in_tz('Europe/Paris')
+    in_paris = utc.in_tz("Europe/Paris")
 
     assert_datetime(in_paris, 2016, 3, 27, 1, 30, 0)
 
@@ -19,7 +19,7 @@ def test_replace_tzinfo_dls_off():
 
 def test_replace_tzinfo_dls_transitioning_on():
     utc = pendulum.datetime(2016, 3, 27, 1, 30)  # In middle of turning on
-    in_paris = utc.in_tz('Europe/Paris')
+    in_paris = utc.in_tz("Europe/Paris")
 
     assert_datetime(in_paris, 2016, 3, 27, 3, 30, 0)
 
@@ -33,7 +33,7 @@ def test_replace_tzinfo_dls_transitioning_on():
 
 def test_replace_tzinfo_dls_on():
     utc = pendulum.datetime(2016, 10, 30, 0, 30)  # 30 min before DLS turning off
-    in_paris = utc.in_tz('Europe/Paris')
+    in_paris = utc.in_tz("Europe/Paris")
 
     assert_datetime(in_paris, 2016, 10, 30, 2, 30, 0)
 
@@ -47,7 +47,7 @@ def test_replace_tzinfo_dls_on():
 
 def test_replace_tzinfo_dls_transitioning_off():
     utc = pendulum.datetime(2016, 10, 30, 1, 30)  # In the middle of turning off
-    in_paris = utc.in_tz('Europe/Paris')
+    in_paris = utc.in_tz("Europe/Paris")
 
     assert_datetime(in_paris, 2016, 10, 30, 2, 30, 0)
 
